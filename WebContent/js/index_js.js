6592178b90ed14ae29dcef7f88062e4406d700b4
zheng7641 <zheng7641@163.com>
2015-11-19 19:49:08 +0800

    15-11-19
    照片墙完成

--- WebContent/js/index_js.js
+++ WebContent/js/index_js.js
@@ -1,19 +1,10 @@
 /**
  * index
  */
-function mouseOut()
-{
-    document.getElementById('subitem').style.display = "block";
-}
-function mouseOver()
-{
-    document.getElementById('subitem').style.display = "block";
-}
 
-function subitem_mouseOver(){
+function mouseOver() {
 	document.getElementById('subitem').style.display = "block";
 }
-function subitem_mouseOut()
-{
-    document.getElementById('subitem').style.display = "none";
+function mouseOut() {
+	document.getElementById('subitem').style.display = "none";
 }
